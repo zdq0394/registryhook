83f4489e41a5555b9ac6fb4cc42fb8401a1ac581
dongqi zhang <zdq123.hn@163.com>
2017-09-19 13:44:32 +0800

    wrap in urfave/cli

--- cmd/hook/main.go
+++ cmd/hook/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"os"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/urfave/cli"
 	"github.com/zdq0394/registryhook/hook"
+	"github.com/zdq0394/registryhook/hook/version"
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "HookServer"
+	app.Version = version.Version.String()
+	app.Flags = []cli.Flag{}
+	app.Action = hookAction
+	if err := app.Run(os.Args); err != nil {
+		logrus.Errorln(err)
+	}
+}
+
+func hookAction(ctx *cli.Context) error {
 	hook.Start()
+	return nil
 }
